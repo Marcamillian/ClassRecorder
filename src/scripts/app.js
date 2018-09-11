4ad83f6fc844b92162acb5e3bba58dfd61a13bf4
Marcamillian <marcamillian@gmail.com>
2018-09-11 10:25:16 +0100

    fix: removing strict from js files

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-'use strict';
+
 
 // tutorial used - https://developer.mozilla.org/en-US/docs/Web/API/MediaStream_Recording_API/Using_the_MediaStream_Recording_API
 const recorderApp = function RecorderApp(){ 
