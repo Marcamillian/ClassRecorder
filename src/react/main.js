91f957456fa51d30f09e6c807c586c360b66cdf0
Marcamillian <marcamillian@gmail.com>
2019-04-17 18:50:10 +0100

    chore: Add react file structure
    - Add react & task directories
      - src
        - * react
          - components
            - title-bar.js
          - main.js
      - * task
        - index.js
        - webpack.js
    
    - install required babel/webpack/react packages

---
+++ src/react/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TitleBar from '../components/title-bar';
+
+ReactDOM.render(
+  <TitleBar />
+  ,document.querySelector('.container')
+)
